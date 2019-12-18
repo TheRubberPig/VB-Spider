1e0e693b3374ce3c8905fe46ece90fbdd7059621
David Cooper <david-cooper32@hotmail.co.uk>
2019-12-18 08:19:23 +0000

    Added the code to look through every directory and find every vb script file

--- vbspider.py
+++ vbspider.py
@@ -1,7 +1,14 @@
-import sys, getopt
+import sys, getopt, os, glob
 
 def main(argv):
-	print ("Hello World!")
+	thisDir = os.getcwd()
+	print("Current Directory is: " + thisDir)
+	for r, d, f in os.walk(thisDir):
+		for file in f:
+			if file.endswith(".vbs"):
+				vbFile = open(os.path.join(r, file))
+				print(r)
+				print(file)
 	
 if __name__ == "__main__":
 	main(sys.argv[1:])
